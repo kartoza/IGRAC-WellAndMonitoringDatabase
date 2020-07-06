c27498771043874010ebf793cf4bc2b1d0d0d230
Anita Hapsari <anita.hapsari15@alumni.imperial.ac.uk>
2020-07-06 13:58:45 +0700

    remove ids

--- sql/vocabularies/well.sql
+++ sql/vocabularies/well.sql
@@ -1,36 +1,36 @@
 -- GW Well --
 -- WellPurposeType --
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (1, 'Extraction', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (2, 'Injection', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (3, 'Observation', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (4, 'Dewatering', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (6, 'Decontamination', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (7, 'Disposal', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (9, 'Geotechnical', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (10, 'Mineral', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (12, 'Monitoring Quality', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (8, 'Flowing Shot', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (11, 'Monitoring Level Head', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (5, 'Cathodic Protection', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (13, 'Oil', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (14, 'Oil Exploratory', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (15, 'Seismic', '');
-INSERT INTO public.gwml2_wellpurposetype (id, name, description) VALUES (16, 'Water Exploratory', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Extraction', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Injection', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Observation', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Dewatering', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Decontamination', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Disposal', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Geotechnical', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Mineral', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Monitoring Quality', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Flowing Shot', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Monitoring Level Head', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Cathodic Protection', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Oil', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Oil Exploratory', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Seismic', '');
+INSERT INTO public.gwml2_wellpurposetype (name, description) VALUES ('Water Exploratory', '');
 
 -- WellStatusTypeTerm --
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (1, 'New', '');
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (2, 'Unfinished', '');
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (3, 'Reconditioned', '');
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (4, 'Deepened', '');
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (5, 'Not in use', '');
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (6, 'Standby', '');
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (7, 'Unknown', '');
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (8, 'Abandoned dry', '');
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (9, 'Abandoned insufficient', '');
-INSERT INTO public.gwml2_wellstatustypeterm (id, name, description) VALUES (10, 'Abandoned quality', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('New', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('Unfinished', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('Reconditioned', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('Deepened', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('Not in use', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('Standby', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('Unknown', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('Abandoned dry', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('Abandoned insufficient', '');
+INSERT INTO public.gwml2_wellstatustypeterm (name, description) VALUES ('Abandoned quality', '');
 
 -- WellWaterUseType --
-INSERT INTO public.gwml2_wellwaterusetype (id, name, description) VALUES (1, 'Agricultural', '');
-INSERT INTO public.gwml2_wellwaterusetype (id, name, description) VALUES (2, 'Domestic', '');
-INSERT INTO public.gwml2_wellwaterusetype (id, name, description) VALUES (3, 'Industrial', '');
-INSERT INTO public.gwml2_wellwaterusetype (id, name, description) VALUES (4, 'Recreation', '');
+INSERT INTO public.gwml2_wellwaterusetype (name, description) VALUES ('Agricultural', '');
+INSERT INTO public.gwml2_wellwaterusetype (name, description) VALUES ('Domestic', '');
+INSERT INTO public.gwml2_wellwaterusetype (name, description) VALUES ('Industrial', '');
+INSERT INTO public.gwml2_wellwaterusetype (name, description) VALUES ('Recreation', '');
