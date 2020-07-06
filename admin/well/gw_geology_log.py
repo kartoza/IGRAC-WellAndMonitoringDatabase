bf233fb989673de8769336475e7c49fdd7f81462
Anita Hapsari <anita@kartoza.com>
2020-07-06 10:19:12 +0700

    fix excel upload (#32)

--- admin/well/gw_geology_log.py
+++ admin/well/gw_geology_log.py
@@ -3,8 +3,12 @@ from gwml2.models.well import GWGeologyLog
 
 
 class Admin(admin.ModelAdmin):
-    list_display = ('phenomenon_time', 'result_time', 'gw_level', 'reference', 'start_depth', 'end_depth')
+    list_display = (
+        'pk', 'phenomenon_time', 'result_time', 'gw_level', 'reference', 'start_depth', 'end_depth', 'well_name')
     filter_horizontal = ('parameter',)
+
+    def well_name(self, obj):
+        return obj.gw_well.gw_well_name
 
 
 admin.site.register(GWGeologyLog, Admin)
