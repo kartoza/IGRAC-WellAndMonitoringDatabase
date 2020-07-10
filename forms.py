313f5c59ed7a9966769e6b9d361cca9e49c3d742
Anita Hapsari <anita.hapsari15@alumni.imperial.ac.uk>
2020-07-10 17:17:21 +0700

    improve excel upload layout

--- forms.py
+++ forms.py
@@ -5,13 +5,13 @@ class CsvWellForm(forms.Form):
     """Form to upload CSV file."""
 
     gw_location_file = forms.FileField(
-        label="Choose GW Location excel File:",
+        label="Choose GW Location Excel File:",
         widget=forms.FileInput(),
         required=False
     )
 
     gw_level_file = forms.FileField(
-        label="Choose GW Level excel File:",
+        label="Choose GW Level Excel File:",
         widget=forms.FileInput(),
         required=False
     )
