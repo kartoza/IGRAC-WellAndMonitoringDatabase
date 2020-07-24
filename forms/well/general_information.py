2a86c0086078a574860bb37e3aeecf603efcc722
meomancer <meomancer@gmail.com>
2020-07-24 14:09:02 +0700

    add map on the information form

--- forms/well/general_information.py
+++ forms/well/general_information.py
@@ -11,8 +11,8 @@ class GeneralInformationForm(forms.Form):
     water_point_type = forms.CharField()
 
     # Location Information
-    latitude = forms.FloatField(help_text='latitude of well')
-    longitude = forms.FloatField(help_text='longitude of well')
+    latitude = forms.FloatField(help_text='latitude of well', min_value=-90, max_value=90)
+    longitude = forms.FloatField(help_text='longitude of well', min_value=-180, max_value=180)
     country = forms.CharField(help_text='country of well')
     address = forms.CharField(
         widget=forms.Textarea, required=False,
