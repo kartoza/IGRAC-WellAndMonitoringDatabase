78b27d07c976aa5279958fe7daca578d6efab7ff
meomancer <meomancer@gmail.com>
2020-10-27 10:11:17 +0700

    fix admin of gwml2

--- admin/well.py
+++ admin/well.py
@@ -7,29 +7,8 @@ from gwml2.models.well import (
 )
 
 
-class WellLevelMeasurementInline(admin.TabularInline):
-    model = WellLevelMeasurement
-
-
-class WellQualityMeasurementInline(admin.TabularInline):
-    model = WellQualityMeasurement
-
-
-class WellYieldMeasurementInline(admin.TabularInline):
-    model = WellYieldMeasurement
-
-
-class WellDocumentInline(admin.TabularInline):
-    model = WellDocument
-
-
 class WellAdmin(admin.ModelAdmin):
     list_display = ('original_id', 'edit')
-    inlines = [
-        WellLevelMeasurementInline,
-        WellQualityMeasurementInline,
-        WellYieldMeasurementInline,
-        WellDocumentInline]
 
     def edit(self, obj):
         url = reverse('well_form', args=[obj.id])
@@ -39,3 +18,29 @@ class WellAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Well, WellAdmin)
+
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('well', 'time', 'parameter', 'methodology', 'value')
+    search_fields = ('well__original_id',)
+
+
+admin.site.register(WellLevelMeasurement, MeasurementAdmin)
+admin.site.register(WellQualityMeasurement, MeasurementAdmin)
+admin.site.register(WellYieldMeasurement, MeasurementAdmin)
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('well', 'uploaded_at', 'file')
+    search_fields = ('well__original_id',)
+
+    def file(self, obj):
+        if obj.file:
+            return format_html(
+                '<a href="{}" target="_blank">click here to edit well</a>',
+                obj.file.url)
+        else:
+            return '-'
+
+
+admin.site.register(WellDocument, DocumentAdmin)
