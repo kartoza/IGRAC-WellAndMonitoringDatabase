778c336b33be819dcdf91ab061cb0b83c96ac5e6
meomancer <meomancer@gmail.com>
2020-10-28 16:23:55 +0700

    add viewers and editors to organisation

--- admin/well_management.py
+++ admin/well_management.py
@@ -1,4 +1,51 @@
+from django import forms
 from django.contrib import admin
 from gwml2.models.well_management.organisation import Organisation
+from django.contrib.auth import get_user_model
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
-admin.site.register(Organisation)
+User = get_user_model()
+
+
+class OrganisationForm(forms.ModelForm):
+    viewer_users = forms.ModelMultipleChoiceField(
+        User.objects.all(),
+        widget=FilteredSelectMultiple('viewer_users', False),
+        required=False
+    )
+    editor_users = forms.ModelMultipleChoiceField(
+        User.objects.all(),
+        widget=FilteredSelectMultiple('editor_users', False),
+        required=False
+    )
+
+    class Meta:
+        model = Organisation
+        fields = ('name', 'description', 'viewer_users', 'editor_users')
+
+    def __init__(self, *args, **kwargs):
+        forms.ModelForm.__init__(self, *args, **kwargs)
+        if self.instance:
+            self.fields['viewer_users'].initial = User.objects.filter(
+                id__in=self.instance.viewers)
+            self.fields['editor_users'].initial = User.objects.filter(
+                id__in=self.instance.editors)
+
+    def save(self, commit=True):
+        instance = super(OrganisationForm, self).save(commit)
+        viewer_users = self.cleaned_data.get('viewer_users', None)
+        if viewer_users is not None:
+            viewer_users = viewer_users.values_list('id', flat=True)
+            instance.viewers = list(viewer_users)
+        editor_users = self.cleaned_data.get('editor_users', None)
+        if editor_users is not None:
+            editor_users = editor_users.values_list('id', flat=True)
+            instance.editors = list(editor_users)
+        return instance
+
+
+class OrganisationAdmin(admin.ModelAdmin):
+    form = OrganisationForm
+
+
+admin.site.register(Organisation, OrganisationAdmin)
--- models/well_management/organisation.py
+++ models/well_management/organisation.py
@@ -1,10 +1,14 @@
-from gwml2.models.term import _Term
+from django.contrib.gis.db import models
+from django.contrib.postgres.fields import ArrayField
 from adminsortable.models import Sortable
+from gwml2.models.term import _Term
 
 
 class Organisation(_Term):
     """ Organisation
     """
+    viewers = ArrayField(models.IntegerField(), default=list, null=True)
+    editors = ArrayField(models.IntegerField(), default=list, null=True)
 
     class Meta(Sortable.Meta):
         db_table = 'organisation'
