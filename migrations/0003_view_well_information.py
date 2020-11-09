00efbb223a737a072dc3dcf06a08ebef28862849
meomancer <meomancer@gmail.com>
2020-11-09 12:33:44 +0700

    update view

--- migrations/0003_view_well_information.py
+++ migrations/0003_view_well_information.py
@@ -9,19 +9,18 @@ class Migration(migrations.Migration):
     ]
 
     sql = """
-       CREATE VIEW well_information AS
-select 
-       w.id,
-       w.original_id                                                  as "original_id",
-       w.name                                                         as "name",
-       c.name                                                         as "country",
-       type.name                                                      as "feature_type",
-       hydro.aquifer_name                                             as "aquifer_name",
+CREATE VIEW well_information AS
+select w.id,
+       w.original_id                as "original_id",
+       w.name                       as "name",
+       c.name                       as "country",
+       type.name                    as "feature_type",
+       array_to_string(ARRAY [0] || admins || editors || viewers || ARRAY [0], ',') as "users",
        w.location
-From well as w
+from well as w
+       LEFT JOIN organisation org on w.organisation_id = org.id
        LEFT JOIN country c on w.country_id = c.id
-       LEFT JOIN term_feature_type type on w.feature_type_id = type.id
-       LEFT JOIN hydrogeology_parameter as hydro on w.hydrogeology_parameter_id = hydro.id;
+       LEFT JOIN term_feature_type type on w.feature_type_id = type.id;
         """
 
     operations = [
