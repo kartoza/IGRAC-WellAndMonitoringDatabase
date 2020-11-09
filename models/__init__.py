a1a8d7d4f420f41bef5ed5c0f6658027e53a6f4a
meomancer <meomancer@gmail.com>
2020-11-09 14:43:27 +0700

    remove download session when something has changed and remove its file

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from gwml2.models.construction import *
+from gwml2.models.download_session import *
 from gwml2.models.drilling import *
 from gwml2.models.general import *
 from gwml2.models.general_information import *
@@ -15,3 +16,9 @@ from gwml2.models.well_management.organisation import *
 
 # for signals models
 from gwml2.signals import *
+
+
+@receiver(post_save)
+def data_deleted(sender, instance, **kwargs):
+    if sender._meta.app_label == 'gwml2' and sender._meta.object_name != 'DownloadSession':
+        DownloadSession.objects.all().delete()
