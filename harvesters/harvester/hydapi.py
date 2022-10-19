9e307b8c47a5e428371dcac4c66244fe2beff349
= <meomancer@gmail.com>
2022-10-19 16:01:23 +0700

    Norway : https://hydapi.nve.no/

--- harvesters/harvester/hydapi.py
+++ harvesters/harvester/hydapi.py
@@ -2,13 +2,14 @@ import pytz
 from dateutil import parser
 from dateutil.relativedelta import relativedelta
 from django.utils import timezone
+
 from gwml2.harvesters.harvester.base import BaseHarvester
 from gwml2.harvesters.models.harvester import Harvester, HarvesterWellData
 from gwml2.models.general import Quantity, Unit
 from gwml2.models.term_measurement_parameter import TermMeasurementParameter
 from gwml2.models.well import (
     MEASUREMENT_PARAMETER_GROUND,
-    Well, WellLevelMeasurement
+    Well, WellLevelMeasurement, WellYieldMeasurement, WellQualityMeasurement
 )
 from gwml2.tasks.well import generate_measurement_cache
 
@@ -23,13 +24,25 @@ class Hydapi(BaseHarvester):
     parameters = {}
     resolution_time = 1440
 
-    def __init__(self, harvester: Harvester, replace: bool = False, original_id: str = None):
+    def __init__(self, harvester: Harvester, replace: bool = False,
+                 original_id: str = None):
         self.parameters = {
+            1001: {
+                'model': WellYieldMeasurement,
+                'parameter': TermMeasurementParameter.objects.get(
+                    name='Spring discharge')
+            },
+            1003: {
+                'model': WellQualityMeasurement,
+                'parameter': TermMeasurementParameter.objects.get(
+                    name='T')
+            },
+            # just get the Instantenous
             5130: {
                 'model': WellLevelMeasurement,
                 'parameter': TermMeasurementParameter.objects.get(
                     name=MEASUREMENT_PARAMETER_GROUND)
-            }
+            },
         }
         super(Hydapi, self).__init__(harvester, replace, original_id)
 
@@ -59,7 +72,8 @@ class Hydapi(BaseHarvester):
             raise Exception('Api key is not provided.')
 
         # fetch stations first
-        stations = self._request_api('https://hydapi.nve.no/api/v1/Stations')['data']
+        stations = self._request_api('https://hydapi.nve.no/api/v1/Stations')[
+            'data']
         for station in stations:
             self._process_station(station)
         self._done('Done')
@@ -71,6 +85,23 @@ class Hydapi(BaseHarvester):
         Save it as HarvesterWellData
         Fetch measurement
         """
+        # check available parameters
+        # check start date
+        parameter_ids = [
+            series['parameter'] for series in station['seriesList']
+        ]
+        series_list = []
+        for series in station['seriesList']:
+            if series['parameter'] in self.parameters.keys():
+                series_list.append(series)
+
+        # Skip if no series that are needed
+        if not series_list:
+            self._update(f"Skip {station['stationId']} : {parameter_ids}")
+            return
+
+        self._update(f"Found {station['stationId']} : {parameter_ids}")
+
         # create well
         if self.original_id and station['stationId'] != self.original_id:
             return
@@ -85,117 +116,123 @@ class Hydapi(BaseHarvester):
         except Well.DoesNotExist:
             return
 
-        # check latest date
-        latest_measurement = WellLevelMeasurement.objects.filter(
-            well=harvester_well_data.well,
-        ).order_by('-time').first()
-
-        if not latest_measurement:
-            harvester_well_data.from_time_data = self.max_oldest_time
-        else:
-            harvester_well_data.from_time_data = latest_measurement.time
-        harvester_well_data.save()
-
-        # check available parameters
-        # check start date
-        series_list = station['seriesList']
-        parameters = []
         for series in series_list:
-            # just get the Instantenous
-            if series['parameter'] in self.parameters:
-                parameters.append(series['parameter'])
-                for resolution in series['resolutionList']:
-                    if resolution['resTime'] == self.resolution_time:
-                        date_from = parser.parse(resolution['dataFromTime'])
-                        if harvester_well_data.from_time_data < date_from:
-                            harvester_well_data.from_time_data = date_from
-                            harvester_well_data.save()
+            # Check the latest from and to date
+            parameter = series['parameter']
+            measurement_parameter = self.parameters[parameter]
+            model = measurement_parameter['model']
 
-        self.fetch_measurements(station, harvester_well_data, parameters)
+            # check latest date
+            latest_measurement = model.objects.filter(
+                well=harvester_well_data.well,
+                parameter=measurement_parameter['parameter']
+            ).order_by('-time').first()
+
+            # Change the from and to time based on the measurements
+            if not latest_measurement:
+                harvester_well_data.from_time_data = self.max_oldest_time
+            else:
+                harvester_well_data.from_time_data = latest_measurement.time
+
+            # CHeck if from date is less than the series
+            for resolution in series['resolutionList']:
+                if resolution['resTime'] == self.resolution_time:
+                    date_from = parser.parse(resolution['dataFromTime'])
+                    if harvester_well_data.from_time_data < date_from:
+                        harvester_well_data.from_time_data = date_from
+
+            harvester_well_data.save()
+
+            self._fetch_measurements(
+                station, harvester_well_data,
+                harvester_well_data.from_time_data, series
+            )
 
         # generate cache
-        generate_measurement_cache(
-            well.id, WellLevelMeasurement.__name__)
+        generate_measurement_cache(well.id, WellLevelMeasurement.__name__)
 
-    def fetch_measurements(
+    def _fetch_measurements(
             self,
             station: dict,
             harvester_well_data: HarvesterWellData,
-            parameters: list):
+            from_date,
+            series: dict):
         """ Processing older measurements """
+        try:
+            parameter = series['parameter']
+            measurement_parameter = self.parameters[parameter]
+            model = measurement_parameter['model']
 
-        # check from and to date
-        from_date = harvester_well_data.from_time_data
-        if harvester_well_data.to_time_data:
-            from_date = harvester_well_data.to_time_data
-        to_date = from_date + relativedelta(months=1)
+            # check from and to date
+            to_date = from_date + relativedelta(months=1)
 
-        is_last = False
-        now = timezone.now()
-        if to_date > now:
-            to_date = now
-            is_last = True
+            is_last = False
+            now = timezone.now()
+            if to_date > now:
+                to_date = now
+                is_last = True
 
-        from_date_str = from_date.astimezone(
-            pytz.utc).replace(microsecond=0).isoformat().split('+')[0] + 'Z'
-        to_date_str = to_date.astimezone(
-            pytz.utc).replace(microsecond=0).isoformat().split('+')[0] + 'Z'
-        self._update('{} : {} - {}'.format(
-            station['stationId'], from_date_str, to_date_str))
-        for parameter in parameters:
-            try:
-                measurement_parameter = self.parameters[parameter]
-                model = measurement_parameter['model']
-                params = [
-                    'StationId={}'.format(station['stationId']),
-                    'Parameter={}'.format(parameter),
-                    'ResolutionTime={}'.format(self.resolution_time),
-                    'ReferenceTime={from_date}/{to_date}'.format(
-                        from_date=from_date_str,
-                        to_date=to_date_str
-                    )
-                ]
-                url = 'https://hydapi.nve.no/api/v1/Observations?{}'.format(
-                    '&'.join(params)
-                )
-                response = self._request_api(url)
-                for data in response['data']:
-                    method = data['method']
-                    try:
-                        unit = Unit.objects.get(name=data['unit'])
-                        for measurement in data['observations']:
-                            if measurement['value'] is not None:
-                                value = measurement['value']
+            from_date_str = from_date.astimezone(
+                pytz.utc
+            ).replace(microsecond=0).isoformat().split('+')[0] + 'Z'
+            to_date_str = to_date.astimezone(
+                pytz.utc
+            ).replace(microsecond=0).isoformat().split('+')[0] + 'Z'
+            self._update(
+                f"{station['stationId']} : {parameter} "
+                f"- {from_date_str} - {to_date_str}"
+            )
 
-                                # specifically based on parameter
-                                if parameter == 5130:
-                                    value = abs(value)
+            # Fetch measurements data
+            params = [
+                f"StationId={station['stationId']}",
+                f"Parameter={parameter}",
+                f"ResolutionTime={self.resolution_time}",
+                f'ReferenceTime={from_date_str}/{to_date_str}'
+            ]
+            url = 'https://hydapi.nve.no/api/v1/Observations?{}'.format(
+                '&'.join(params)
+            )
+            response = self._request_api(url)
+            for data in response['data']:
+                method = data['method']
+                try:
+                    unit = Unit.objects.get(name=data['unit'])
+                    for measurement in data['observations']:
+                        if measurement['value'] is not None:
+                            value = measurement['value']
 
-                                defaults = {
-                                    'methodology': method,
-                                    'parameter': measurement_parameter['parameter']
-                                }
-                                if model == WellLevelMeasurement:
-                                    defaults['value_in_m'] = value
-                                obj = self._save_measurement(
-                                    model,
-                                    parser.parse(measurement['time']),
-                                    defaults,
-                                    harvester_well_data
+                            # specifically based on parameter
+                            if parameter == 5130:
+                                value = abs(value)
+
+                            defaults = {
+                                'methodology': method,
+                                'parameter': measurement_parameter['parameter']
+                            }
+                            if model == WellLevelMeasurement:
+                                defaults['value_in_m'] = value
+                            obj = self._save_measurement(
+                                model,
+                                parser.parse(measurement['time']),
+                                defaults,
+                                harvester_well_data
+                            )
+                            if not obj.value:
+                                obj.value = Quantity.objects.create(
+                                    unit=unit,
+                                    value=value
                                 )
-                                if not obj.value:
-                                    obj.value = Quantity.objects.create(
-                                        unit=unit,
-                                        value=value
-                                    )
-                                    obj.save()
+                                obj.save()
 
-                    except Unit.DoesNotExist as e:
-                        print('{}'.format(e))
-            except KeyError:
-                pass
-        harvester_well_data.to_time_data = to_date
-        harvester_well_data.save()
+                except Unit.DoesNotExist as e:
+                    print(f'{e}')
+            harvester_well_data.from_time_data = to_date
+            harvester_well_data.save()
 
-        if not is_last:
-            self.fetch_measurements(station, harvester_well_data, parameters)
+            if not is_last:
+                self._fetch_measurements(
+                    station, harvester_well_data, to_date, series
+                )
+        except KeyError:
+            pass
