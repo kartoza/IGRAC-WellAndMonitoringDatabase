648b7c6e3017c7d6672ab38482b67265014fcff5
Irwan Fathurrahman <meomancer@gmail.com>
2024-05-08 11:41:13 +0700

    Fix clean download data task

--- tasks/clean.py
+++ tasks/clean.py
@@ -2,7 +2,6 @@ import os
 
 from celery import shared_task
 from celery.utils.log import get_task_logger
-from django.utils import timezone
 
 from gwml2.models.download_request import DownloadRequest
 
@@ -11,13 +10,12 @@ logger = get_task_logger(__name__)
 
 @shared_task(
     bind=True,
-    name='gwml2.tasks.harvester.clean_download_file',
+    name='gwml2.tasks.clean.clean_download_file',
     queue='geoserver.events')
 def clean_download_file(self):
     """Run clean download file."""
     for download in DownloadRequest.objects.all():
-        diff = timezone.now() - download.request_at
-        if diff.days >= 1:
+        if download.age_hours >= 5:
             _file = download.file()
             if _file:
                 os.remove(_file)
