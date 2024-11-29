839c5f38b196264074738fbec40165f4db6256c5
Irwan Fathurrahman <meomancer@gmail.com>
2024-11-29 17:21:19 +0700

    Put init data on update batch

--- tasks/uploader/management_data.py
+++ tasks/uploader/management_data.py
@@ -2,6 +2,7 @@ from celery.utils.log import get_task_logger
 
 from gwml2.models.term import TermGroundwaterUse
 from gwml2.tasks.uploader.base import BaseUploader
+from gwml2.utils.well_data import WellData
 
 logger = get_task_logger(__name__)
 
@@ -48,4 +49,26 @@ class ManagementUploader(BaseUploader):
     def get_object(self, sheet_name, well, record):
         """ return object that will be used
         """
-        return None
+        try:
+            return well.management
+        except Exception:
+            return None
+
+    def update_with_init_data(self, well, record):
+        """Convert record."""
+        init_data = WellData(
+            well,
+            feature_types=self.feature_types, purposes=self.purposes,
+            status=self.status, units=self.units,
+            organisations=self.organisations,
+            groundwater_uses=self.groundwater_uses,
+            confinements=self.confinements,
+            aquifer_types=self.aquifer_types
+        )
+        for key, value in init_data.management().items():
+            if record['management'][key] in ["", None]:
+                record['management'][key] = value
+        for key, value in init_data.management_license().items():
+            if record['management']['license'][key] in ["", None]:
+                record['management']['license'][key] = value
+        return record
