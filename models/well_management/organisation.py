b1509d5e77d489a0cbd472cb56aeb04155a2c984
Irwan Fathurrahman <meomancer@gmail.com>
2025-04-11 08:59:29 +0700

    Fix ugettext

--- models/well_management/organisation.py
+++ models/well_management/organisation.py
@@ -1,6 +1,6 @@
 from django.contrib.gis.db import models
 from django.contrib.postgres.fields import ArrayField
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from gwml2.models.general import Country
 
