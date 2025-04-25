c1f9976fc618a98f8abb4f86eae7a9a871b5f6f0
Irwan Fathurrahman <meomancer@gmail.com>
2025-04-25 13:29:46 +0700

    Improve upload (#162)
    * Improve report generator
    
    * Improve upload resume
    
    * Fix ods format

--- tasks/uploader/task.py
+++ tasks/uploader/task.py
@@ -16,3 +16,13 @@ def well_batch_upload(self, upload_session_id: str, restart: bool = False):
         upload_session.run(restart)
     except UploadSession.DoesNotExist:
         logger.debug('Upload session does not exists')
+
+
+@shared_task(bind=True, queue='update')
+def well_batch_upload_create_report(self, upload_session_id: str, restart: bool = False):
+    try:
+        upload_session = UploadSession.objects.get(id=upload_session_id)
+        upload_session.create_report_excel()
+        upload_session.save()
+    except UploadSession.DoesNotExist:
+        logger.debug('Upload session does not exists')
