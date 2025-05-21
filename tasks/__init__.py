3873cd99b77ae14402f4757611e5b35c28366d16
Irwan Fathurrahman <meomancer@gmail.com>
2025-05-21 15:49:40 +0700

    Move update ggis_uid to background

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -6,6 +6,7 @@ from gwml2.tasks.data_file_cache import (
 )
 from gwml2.tasks.downloader import prepare_download_file
 from gwml2.tasks.harvester import run_harvester, run_all_harvester
-from gwml2.tasks.uploader.task import well_batch_upload
+from gwml2.tasks.organisation import update_ggis_uid
 from gwml2.tasks.upload_session import resume_all_uploader
+from gwml2.tasks.uploader.task import well_batch_upload
 from gwml2.tasks.well import generate_measurement_cache
