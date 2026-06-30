bc5d6b963ec07dfe4cf84deee6c9e1f534f8c0fb
Irwan Fathurrahman <meomancer@gmail.com>
2026-06-30 12:27:51 +0700

    Fix country data cache should be split between ggmn and non (#243)
    * Fix country data cache should be split between ggmn and non
    
    * Add timeout when it is ready

--- tasks/well_file_cache/country_recache.py
+++ tasks/well_file_cache/country_recache.py
@@ -3,10 +3,10 @@ import os
 
 from celery import shared_task
 from django.conf import settings
-from django.utils import timezone
 
 from gwml2.models.general import Country
 from gwml2.models.well import Well
+from gwml2.models.well_management.organisation import OrganisationGroup
 from gwml2.tasks.well_file_cache.well_zipped_cache import WellZippedCache
 
 COUNTRY_DATA_FOLDER = os.path.join(settings.GWML2_FOLDER, 'country-data')
@@ -16,20 +16,35 @@ class GenerateCountryCacheFile(WellZippedCache):
     """Generate country cache file."""
     data_folder = COUNTRY_DATA_FOLDER
     country = None
+    is_ggmn = None
 
     @property
     def cache_name(self) -> str:
         """Return name of cache object."""
+        if self.is_ggmn:
+            return f'{self.country.code}-ggmn'
         return self.country.code
 
     @property
     def well_queryset(self):
-        return Well.objects.filter(
+        queryset = Well.objects.filter(
             country=self.country
-        ).filter(organisation__isnull=False).order_by('original_id')
+        ).filter(organisation__isnull=False)
+        group = OrganisationGroup.get_ggmn_group()
+        if group:
+            if self.is_ggmn:
+                queryset = queryset.filter(
+                    organisation__in=group.organisations.all()
+                )
+            else:
+                queryset = queryset.exclude(
+                    organisation__in=group.organisations.all()
+                )
+        return queryset.order_by('original_id')
 
-    def __init__(self, country):
+    def __init__(self, country, is_ggmn):
         self.country = country
+        self.is_ggmn = is_ggmn
 
     def run(self):
         super(GenerateCountryCacheFile, self).run()
@@ -39,22 +54,37 @@ class GenerateCountryCacheFile(WellZippedCache):
         )
         _file = os.path.join(
             self.data_folder,
-            f'{str(self.cache_name)} - metadata.json'
+            f'{self.country.code} - metadata.json'
         )
-        with open(_file, "w") as outfile:
-            outfile.write(
-                json.dumps(
-                    {'organisations': list(set(organisations))},
-                    indent=4)
-            )
+        metadata = {}
+        if os.path.exists(_file):
+            try:
+                with open(_file, 'r') as f:
+                    metadata = json.load(f)
+            except (json.JSONDecodeError, OSError):
+                metadata = {}
+
+        if self.is_ggmn:
+            metadata['organisations-ggmn'] = list(set(organisations))
+        else:
+            metadata['organisations'] = list(set(organisations))
+
+        with open(_file, 'w') as outfile:
+            outfile.write(json.dumps(metadata, indent=4))
 
 
 @shared_task(bind=True, queue='update')
 def generate_data_country_cache(self, country_code: str):
     try:
         country = Country.objects.get(code__iexact=country_code)
-        generator = GenerateCountryCacheFile(country)
+        # For non GGMN
+        generator = GenerateCountryCacheFile(country, False)
         generator.run()
+
+        # For GGMN
+        generator = GenerateCountryCacheFile(country, True)
+        generator.run()
+
         country.assign_data()
     except Country.DoesNotExist:
         print('Country not found')
